0619c3a9f04d0be4a7416a6541a06a0448a3c979
Harel0 <66210455+Harel0@users.noreply.github.com>
2020-09-08 17:36:20 +0300

    Add files via upload

---
+++ esx_doorlock/__resource.lua
@@ -0,0 +1,13 @@
+--This Script Converted By Elior#0590, Mr.Itchy#2223.
+
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+server_scripts {
+	'config.lua',
+	'server/main.lua'
+}
+
+client_scripts {
+	'config.lua',
+	'client/main.lua'
+}
